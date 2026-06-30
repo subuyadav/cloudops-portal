5bdcf06e051e545c69bee4960c6a09731e175b3e
Subhash Yadav <subuyadav25@gmail.com>
2026-06-30 13:40:58 +0530

    Sprint 8 Phase 1 - Added reusable audit logging utility

---
+++ app/utils/audit.py
@@ -0,0 +1,25 @@
+from database.db import db
+from app.models.audit_log import AuditLog
+
+
+def log_action(username, action, module):
+    """
+    Save an audit record.
+    """
+
+    try:
+
+        log = AuditLog(
+            username=username,
+            action=action,
+            module=module
+        )
+
+        db.session.add(log)
+        db.session.commit()
+
+    except Exception as ex:
+
+        db.session.rollback()
+
+        print(f"Audit Log Error : {ex}")
